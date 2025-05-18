7b50c6b5d13a92c842701b45e60f3a8495a28895
Aida <aida.hosseinian@yahoo.com>
2025-05-18 15:25:23 +0200

    delete Extra comments

--- src/todo-container/TodoWrapper.jsx
+++ src/todo-container/TodoWrapper.jsx
@@ -8,36 +8,7 @@ import {addTodo} from "../store/reducer";
 
 
 function TodoWrapper({todo}) {
-   //const[todoList, setTodoList]= useState([])
    const [store, dispatch] = useStore();
-
-//    const handleAddTodo = () => {
-//     dispatch(addTodo({
-//         id: (((1+Math.random())*0x10000)|0).toString(16).substring(1),
-//         task: todo,
-//         isComplete: false,
-//         isEdit: false
-//     }))
-//    }
-//    console.log(todo)
-//   /*  const addTodo =(todoTxt) => {
-//     setTodoList([...todoList, {
-//         id: (((1+Math.random())*0x10000)|0).toString(16).substring(1),
-//         task: todoTxt,
-//         isComplete: false,
-//         isEdit: false
-//     }])
-//    } */
-
-//    const handleComplete = (id)=> {
-//     setTodoList([...todoList.map(todo => todo.id === id ? {...todo, isComplete: !todo.isComplete} : todo)])
-//    }
-
-//   
-
-//    const handleEdit = (id, newTask)=> {
-//     setTodoList([...todoList.map(todo => todo.id === id ? {...todo, task: newTask} : todo)])
-//    }
      
     return (<div className="todo-wrapper">   
         <div className="todo-task-bar"> 
@@ -47,9 +18,6 @@ function TodoWrapper({todo}) {
                 {store.todoList.map(todo => <TodoItem 
                 {...todo} 
                 key={todo.id}
-                // onComplete={handleComplete}
-                // onDelete={handleDelete}
-                // onEdit={handleEdit}
                 />)}
             </ul>
         </div>
